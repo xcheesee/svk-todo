c57688d3e55682b2e53f635620172e32e558e199
Joao Lucas Silva <joaolucas@prefeitura.sp.gov.br>
2023-03-14 10:27:38 -0300

    criado estilo de hanging tab

---
+++ src/components/TopTab.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let tabName: string;
+    export let active: boolean = false;
+    export let setActive: () => void;
+    export let pos: number;
+</script>
+
+<div class={`relative ${active ? "z-30" : ""}`} on:mouseover={setActive} on:focus={setActive}>
+    <div class={`${active ? "bg-neutral-300 z-30" : "bg-red-500"} px-5 rounded-t-md relative`}>{tabName}</div>
+    <div class={`eased-out -left-[10px] ${active ? "block" : "hidden"}`}>
+        <div class="square top-[20px] relative bg-neutral-300"></div>
+        <div class={`circle relative -left-[10px] ${pos === 0 ? "bg-white" : "bg-red-500"}`}></div>
+    </div>
+    <div class={`eased-out -right-[20px] ${active ? "block" : "hidden"}`}>
+        <div class="square top-[20px] relative bg-neutral-300"></div>
+        <div class={`circle relative ${pos === 2 ? "bg-white" : "bg-red-500"}`}></div>
+    </div>
+</div>
+
+<style>
+    .eased-out {
+        position: absolute;
+        bottom: 0;
+    }
+    .square {
+        width:10px;
+        height: 10px;
+    }
+    .circle {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,19 @@
  <script lang="ts">
     import "iconify-icon"
 	import DayTodo from "../components/DayTodo.svelte";
+	import TopTab from "../components/TopTab.svelte";
     let selectedTag: number = 0;
     let datas: string[] = ["Dia", "Mes", "Ano"]
  </script>   
     
     
 <div class="grid grid-cols-[200px_1fr] grid-rows-[max-content_1fr] lg:h-[800px] lg:w-[1500px] self-center justify-self-center relative rounded-r-lg">
-    <div class="col-start-2 flex gap-4 tag-selector">
+    <div class="col-start-2 flex tag-selector mx-2">
         {#each datas as data, i}
             {#if selectedTag === i}
-            <p 
-                class={`bg-red-500`} 
-                on:mouseover={() => selectedTag = i} 
-                on:focus={() => selectedTag = i}>
-                {data}
-            </p>
+            <TopTab active setActive={() => {selectedTag = i}} pos={i} tabName={data}/>
             {:else}
-            <p 
-                class={`${selectedTag === i - 1 || selectedTag === i + 1 ? "bg-neutral-500" : ""}`} 
-                on:mouseover={() => selectedTag = i} 
-                on:focus={() => selectedTag = i}>
-                {data}
-            </p>
+            <TopTab setActive={() => {selectedTag = i}} pos={i} tabName={data}/>
             {/if}
         {/each}
     </div>
@@ -34,9 +25,9 @@
         </div>
     </div>
     <div class="row-start-2 bg-neutral-300 rounded-r-xl grid grid-cols-4 py-8 px-4 gap-4 relative overflow-y-auto w-full h-full">
-        {#each Array(3) as bruh}
+        { #each Array(3) as _ }
             <DayTodo title="pog" />
-        {/each}
+        { /each }
         <div class="absolute right-0 bg-white rounded-[50%] w-8 h-8 flex justify-center items-center m-2">
             <iconify-icon icon="ic:baseline-plus" width="24"/>
         </div>
